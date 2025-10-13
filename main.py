ce727c67126777570ba4240eaef61673fb51a94d
Zhanibek Sultanbek <starvoid@I-am-Human.local>
2025-10-13 21:31:22 +0500

    Now can download instagram videos in mp4 and audio in mp3

--- main.py
+++ main.py
@@ -1,65 +1,81 @@
 from fastapi import FastAPI, Query
 from fastapi.responses import StreamingResponse
 from starlette.responses import PlainTextResponse
-
 from database import log_download
 from services import Services, clean_youtube_url
 
 app = FastAPI(title="Media Porter")
 
 
-@app.get("/mp3/download")
-def download_mp3(url: str = Query(..., description="YouTube URL")):
+@app.get("/download/youtube/mp3")
+def download_youtube_mp3(url: str = Query(..., description="YouTube video URL")):
     try:
         cleaned_url = clean_youtube_url(url)
         print(f"{cleaned_url = }")
-        mp3_bytes = Services.youtube_downloader.download_mp3(url=cleaned_url)
 
-        log_download(url=cleaned_url, fmt='mp3')
+        mp3_bytes = Services.youtube_downloader.download_mp3(url=cleaned_url)
+        log_download(url=cleaned_url, fmt="youtube-mp3")
 
         return StreamingResponse(
             content=mp3_bytes,
             media_type="audio/mpeg",
-            headers={"Content-Disposition": 'attachment; filename="download.mp3"'}
+            headers={"Content-Disposition": 'attachment; filename="youtube_audio.mp3"'}
         )
     except Exception as e:
-        print(f"Exception in download_mp3() - {e}")
+        print(f"Exception in download_youtube_mp3() - {e}")
         return PlainTextResponse(content="Internal server exception", status_code=500)
 
 
-@app.get("/mp4/download")
-def download_mp4(url: str = Query(..., description="YouTube URL")):
+@app.get("/download/youtube/mp4")
+def download_youtube_mp4(url: str = Query(..., description="YouTube video URL")):
     try:
         cleaned_url = clean_youtube_url(url)
         print(f"{cleaned_url = }")
-        mp4_bytes = Services.youtube_downloader.download_mp4(url=cleaned_url)
 
-        log_download(url=cleaned_url, fmt='mp4')
+        mp4_bytes = Services.youtube_downloader.download_mp4(url=cleaned_url)
+        log_download(url=cleaned_url, fmt="youtube-mp4")
 
         return StreamingResponse(
             content=mp4_bytes,
             media_type="video/mp4",
-            headers={"Content-Disposition": 'attachment; filename="download.mp4"'}
+            headers={"Content-Disposition": 'attachment; filename="youtube_video.mp4"'}
         )
     except Exception as e:
-        print(f"Exception in download_mp4() - {e}")
+        print(f"Exception in download_youtube_mp4() - {e}")
         return PlainTextResponse(content="Internal server exception", status_code=500)
-    
 
-@app.get("/instagram/download")
-def download_instagram(url: str = Query(..., description="Instagram video URL")):
+
+@app.get("/download/instagram/mp4")
+def download_instagram_mp4(url: str = Query(..., description="Instagram video URL")):
     try:
         print(f"{url = }")
-        video_bytes = Services.youtube_downloader.download_instagram(url=url)
 
-        log_download(url=url, fmt='instagram')
+        mp4_bytes = Services.youtube_downloader.download_instagram(url=url)
+        log_download(url=url, fmt="instagram-mp4")
 
         return StreamingResponse(
-            content=video_bytes,
+            content=mp4_bytes,
             media_type="video/mp4",
             headers={"Content-Disposition": 'attachment; filename="instagram_video.mp4"'}
         )
     except Exception as e:
-        print(f"Exception in download_instagram() - {e}")
+        print(f"Exception in download_instagram_mp4() - {e}")
         return PlainTextResponse(content="Internal server exception", status_code=500)
 
+
+@app.get("/download/instagram/mp3")
+def download_instagram_mp3(url: str = Query(..., description="Instagram video URL")):
+    try:
+        print(f"{url = }")
+
+        mp3_bytes = Services.youtube_downloader.download_instagram_mp3(url=url)
+        log_download(url=url, fmt="instagram-mp3")
+
+        return StreamingResponse(
+            content=mp3_bytes,
+            media_type="audio/mpeg",
+            headers={"Content-Disposition": 'attachment; filename="instagram_audio.mp3"'}
+        )
+    except Exception as e:
+        print(f"Exception in download_instagram_mp3() - {e}")
+        return PlainTextResponse(content="Internal server exception", status_code=500)
